09b24cb1854db4a34769c8f43e2c8d6dec1eeaa9
Rivenshell <rivenshell111@gmail.com>
2022-11-14 17:19:54 -0500

    OpenScript

--- src/js/component/footer.js
+++ src/js/component/footer.js
@@ -2,9 +2,16 @@ import React, { Component } from "react";
 
 export const Footer = () => (
 	<footer className="footer mt-auto py-3 text-center">
+		<br />
+		<br />
+		<br />
+		<br />
+		<br />
+		<br />
+		<br />
 		<p>
 			Made with <i className="fa fa-heart text-danger" /> by{" "}
-			<a href="http://www.4geeksacademy.com">4Geeks Academy</a>
+			<a href="http://www.4geeksacademy.com">rivenshell salomon</a>
 		</p>
 	</footer>
 );
--- src/js/views/home.js
+++ src/js/views/home.js
@@ -4,12 +4,13 @@ import "../../styles/home.css";
 
 export const Home = () => (
 	<div className="text-center mt-5">
-		<h1>Hello Rigo!</h1>
+		<h1> <b>Welcome</b> Explorer</h1>
 		<p>
-			<img src={rigoImage} />
+			<img src="https://images.unsplash.com/photo-1660663374499-9cbf2415ffb9?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2232&q=80"
+			width={"1140px"} />
 		</p>
-		<a href="#" className="btn btn-success">
+		{/* <a href="#" className="btn btn-success">
 			If you see this green button, bootstrap is working
-		</a>
+		</a> */}
 	</div>
 );
